76b9d808c3edb8e4aa0431a72832cc55ad8b0623
Nuke-Monkey <126367570+Nuke-Monkey@users.noreply.github.com>
2024-04-07 17:50:09 -0400

    Added equation cleanup function

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 //first commit
 
-var Equation = ""
+var Equ = ""
 
-var operations = ["+", "-", "*", "/", "^", "(", ")"]
+var operations = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "p", "+", "-", "*", "/", "^", "(", ")"]
 
-for (let i = 0; i < 24; i++) {
-    Equation + operations[Math.round(Math.random * 5)];
+
+function equ_init(){
+    for (let i = 0; i < 15; i++) {
+        Equ = Equ + operations[Math.round(Math.random() * 14)];
+    }
+    
 }
+
+function equ_clean(){
+    let EquPreview = ""
+    for (let i = 0; i < Equ.length; i++) {
+        console.log(Equ[i], operations.indexOf(Equ[i]))
+        if (!(operations.indexOf(Equ[i]) > 10 && operations.indexOf(Equ[i-1]) > 10)) {
+            EquPreview = EquPreview + Equ[i]
+        }
+    }
+
+
+    Equ = EquPreview
+}
+
+
+equ_init()
+console.log(Equ)
+equ_clean()
+console.log(Equ)
