cd50c8d2cf9e3faa6c38c67fbfeafbcf1025dba7
Nuke-Monkey <126367570+Nuke-Monkey@users.noreply.github.com>
2024-04-06 20:52:10 -0400

    First Commit

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+//first commit
 
+var Equation = ""
+
+var operations = ["+", "-", "*", "/", "^", "(", ")"]
+
+for (let i = 0; i < 24; i++) {
+    Equation + operations[Math.round(Math.random * 5)];
+}
